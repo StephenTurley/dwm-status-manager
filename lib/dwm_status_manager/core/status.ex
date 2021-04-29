529ec457b5ac28e0d1cc4e51d2f3917ff6578d78
Stephen Turley <stephen.turley@blockfi.com>
2021-04-29 10:51:41 -0400

    added value updates

--- lib/dwm_status_manager/core/status.ex
+++ lib/dwm_status_manager/core/status.ex
@@ -1,16 +1,17 @@
 defmodule DwmStatusManager.Core.Status do
-  defstruct ~w[delimeter components values processor]a
+  defstruct ~w[delimiter components values processor]a
 
   def new(fields) do
-    struct!(
-      __MODULE__,
-      fields
-      |> Keyword.put(:components, [])
-      |> Keyword.put(:values, [])
-    )
+    struct!(__MODULE__, fields)
+    |> update_values()
   end
 
   def to_string(status) do
-    Enum.map_join(status.values, status.delimeter)
+    Enum.join(status.values, status.delimiter)
+  end
+
+  def update_values(status) do
+    status
+    |> Map.put(:values, Enum.map(status.components, status.processor))
   end
 end
--- test/dwm_status_manager/core/status_test.exs
+++ test/dwm_status_manager/core/status_test.exs
@@ -1,20 +1,41 @@
 defmodule DwmStatusManager.Core.StatusTest do
   use ExUnit.Case
   alias DwmStatusManager.Core.Status
+  alias DwmStatusManager.Core.Component
 
-  def processor(component) do
+  defp processor(component) do
     "#{component.command} #{component.interval}"
   end
 
-  def build_status do
-    Status.new(delimeter: '|', processor: &processor/1)
-  end
-
-  def build_status(components) do
-    Status.new(delimeter: '|', components: components, processor: &processor/1)
+  defp build_status(components) do
+    Status.new(delimiter: "|", components: components, processor: &processor/1)
   end
 
   test "no components yeilds no status" do
-    assert build_status() |> Status.to_string() == ""
+    assert build_status([]) |> Status.to_string() == ""
+  end
+
+  test "it builds a single component" do
+    component = Component.new(command: "flerpn", interval: 500)
+
+    result =
+      build_status([component])
+      |> Status.to_string()
+
+    assert result == "flerpn 500"
+  end
+
+  test "it builds multiple components" do
+    components = [
+      Component.new(command: "flerpn", interval: 500),
+      Component.new(command: "derpn", interval: 10_000),
+      Component.new(command: "herpn", interval: 1_000)
+    ]
+
+    result =
+      build_status(components)
+      |> Status.to_string()
+
+    assert result == "flerpn 500|derpn 10000|herpn 1000"
   end
 end
